0983035cbd78726ff5e26b5906de11e84543160e
CGHuy <164297800+CGHuy@users.noreply.github.com>
2024-06-27 20:49:56 +0700

    Add files via upload

---
+++ bai15.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#include <string.h>
+
+main () {
+	int a, b;
+	char ch;
+	printf ("Nhap vao a, b: ");
+	scanf ("%d %d",&a,&b);
+	printf ("Lua chon phep tinh: ");
+	fflush(stdin);
+	ch = getchar();
+	switch(ch) {
+		case '+' :
+			printf ("Tong la: %d",a+b);
+			break;
+		case '-' :
+			printf ("Hieu la: %d",a-b);
+			break;
+		case '*' :
+			printf ("Tich la: %d",a*b);
+			break;
+		case '/' :
+			printf ("Thuong la: %.1f",float(a/b));
+			break;
+	}
+	getch();
+}
