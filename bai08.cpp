743c3b279d59a3b2b1b0135ae43074a5f96ea218
CGHuy <164297800+CGHuy@users.noreply.github.com>
2024-06-27 22:04:30 +0700

    Update bai08.cpp

--- bai08.cpp
+++ bai08.cpp
@@ -6,10 +6,9 @@ main () {
 	int so;
 	printf ("Nhap so: ");
 	scanf ("%d", &so);
-	int hangtram = so / 100;
-	int phanduhangtram = so % 100;
-	int hangchuc = phanduhangtram / 10;
-	int phanduhamchuc = phanduhangtram % 10;
+	int hangtram = so/100;
+	int hangchuc = (so%100)/10;
+	int hangdonvi = so%100;
 	printf ("\nChu so hang tram: %d", hangtram);
 	printf ("\nChu so hang chuc: %d", hangchuc);
 	printf ("\nChu so hang don vi: %d", phanduhamchuc);
