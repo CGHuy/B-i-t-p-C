b8b7fea0493c496de780cccf3296209d87eac29d
CGHuy <164297800+CGHuy@users.noreply.github.com>
2024-06-27 20:52:00 +0700

    Add files via upload

---
+++ bai8.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+
+main () {
+	int so;
+	printf ("Nhap so: ");
+	scanf ("%d", &so);
+	int hangtram = so / 100;
+	int phanduhangtram = so % 100;
+	printf ("\nChu so hang tram: %d", hangtram);
+	int hangchuc = phanduhangtram / 10;
+	int phanduhamchuc = phanduhangtram % 10;
+	printf ("\nChu so hang chuc: %d", hangchuc);
+	printf ("\nChu so hang don vi: %d", phanduhamchuc);
+	getch();
+}
