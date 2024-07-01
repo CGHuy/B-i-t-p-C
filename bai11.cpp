c03ee80619be9b6d17e6147ad06089315bcc2ad8
CGHuy <164297800+CGHuy@users.noreply.github.com>
2024-07-01 20:25:39 +0700

    Update bai11.cpp

--- bai11.cpp
+++ bai11.cpp
@@ -15,7 +15,7 @@ main () {
 				printf ("Phuong trinh vo nhiem");
 			}
 		} else {
-			printf ("Phuong trinh co 1 nhiem duy nhat %.2f",-b/2a);
+			printf ("Phuong trinh co 1 nhiem duy nhat %.2f",-b/2*a);
 		}
 	} else {
 		delta = pow(b,2) - 4*a*c;
