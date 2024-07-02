96517fa1815ca6aeb1c6e4f2952d369c26141b1a
CGHuy <164297800+CGHuy@users.noreply.github.com>
2024-07-02 16:47:45 +0700

    Add files via upload

---
+++ bai37.cpp
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+struct sach {
+	char masach[10];
+	char tensach[50];
+	char tacgia[50];
+	int namxb;
+};
+
+sach nhap() {
+	sach a;
+	printf ("\nNhap ma sach: "); fflush(stdin); gets(a.masach);
+	printf ("Nhap ten sach: "); fflush(stdin); gets(a.tensach);
+	printf ("Nhap ten tac gia: "); fflush(stdin); gets(a.tacgia);
+	printf ("Nhap nam xuat ban: "); scanf ("%d",&a.namxb);
+	return a;
+}
+
+void xuat(sach a) {
+	printf ("%-5s%-20s%-20s%4d\n",a.masach,a.tensach,a.tacgia,a.namxb);
+}
+
+int main() {
+	sach a[100];
+	int n;
+	printf ("Nhap so sach: "); scanf ("%d",&n);
+	for (int i=0; i<n; i++) {
+		a[i] = nhap();
+	}
+	
+	printf ("\nNhung quyen sach co ten tin hoc dai cuong\n");
+	for (int i=0; i<n; i++) {
+		if (strstr("tin hoc dai cuong",a[i].tensach)) {
+			xuat(a[i]);
+		}
+	}
+	
+	printf ("\nDanh sach cac quyen sach theo thu tu\n");
+	for (int i=0; i<n-1; i++) {
+		for (int j=0; j<n; j++) {
+			if (strcmp(a[i].tensach,a[j].tensach) > 0) {
+				sach temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+			}
+		}
+	}
+	for (int i=0; i<n; i++) {
+		xuat(a[i]);	
+	}
+	
+	getch();
+}
